d40a18ca97759826a779bb897bee90547f685069
Harsha R Mundaragi <harsha.mundaragi@robosoftin.com>
2023-10-19 16:13:26 +0530

    new01 Commit

--- JustForGitPractice/ViewController.swift
+++ JustForGitPractice/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hi"
+        label.textColor = .darkText
+        label.backgroundColor = .systemGray4
+        label.textAlignment = .center
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // main branch first commit
         // branch 01 commit
+        view.addSubview(label)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        label.frame = CGRect(x: (UIScreen.main.bounds.width - 100)/2, y: (UIScreen.main.bounds.height - 40)/2, width: 100, height: 40)
     }
 
 
